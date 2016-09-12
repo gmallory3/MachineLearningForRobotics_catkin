ed73c00db912f8ac0830ba5315671effa8b1ff54
MALLORY <gmallory@gtlpc118.georgiatech-metz.fr>
2016-09-12 17:38:23 +0200

    Temp commit so Garrett can pull from Jonas's recent push

--- learning_joy/src/learning_joy.py
+++ learning_joy/src/learning_joy.py
@@ -6,24 +6,33 @@ from sensor_msgs.msg import Joy
 twist = Twist()
 
 def callback(data):
-	
     twist.linear.x = 1*data.axes[1]
     #twist.angular.z = 4*data.axes[0]
     #pub.publish(twist)
     
+def twistStatus_callback(data):
+	pub.publish(twist)
+
 
 def start():
+    global pub
+    global pub_2
     
     rospy.init_node('Joy_robot')
-    #global pub
+    
     rospy.Subscriber("~input", Joy, callback)
+	rospy.Subscriber("/vrep/twistStatus", Twist, twistStatus_callback)
+	
     pub = rospy.Publisher('~output', Twist)
-    rate = rospy.Rate(60)
+    pub_2 = rospy.Publisher("/vrep/twistStatus_2", Twist)
+    
+    """
+    rate = rospy.Rate(34) # 60 hz or 34 to match publish rate of /vrep/twistStatus
     while not rospy.is_shutdown():
 		pub.publish(twist)
 		rate.sleep()
-    
-    #rospy.spin()
+    """
+    rospy.spin()
 
 if __name__ == '__main__':
     start()
