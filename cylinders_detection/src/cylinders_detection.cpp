8a7675ea0a0d6259c9555dc032ed141eca80ee0f
LOY <jloy@gtlpc108.georgiatech-metz.fr>
2016-10-05 17:57:24 +0200

    Functional cylinder detection in the cylinder detection package. Need to tune parameters so it fits column better. Need to not display marker when not looking at a cylinder.

--- cylinders_detection/src/cylinders_detection.cpp
+++ cylinders_detection/src/cylinders_detection.cpp
@@ -15,7 +15,7 @@
 #include <Eigen/Dense>
 
 
-class FloorPlaneRansac {
+class CylindersDetection {
     protected:
         ros::Subscriber scan_sub_;
         ros::Publisher marker_pub_;
@@ -28,6 +28,7 @@ class FloorPlaneRansac {
         double tolerance;
         int n_samples; 
 
+        double best_radius;
         pcl::PointCloud<pcl::PointXYZ> lastpc_;
 
     protected: // ROS Callbacks
@@ -68,95 +69,82 @@ class FloorPlaneRansac {
             n = pidx.size();
             size_t best = 0;
             double X[3] = {0,0,0};
+              
             ROS_INFO("%d useful points out of %d",(int)n,(int)temp.size());
             
             
             for (unsigned int i=0;i<(unsigned)n_samples;i++) {
                 // Implement RANSAC here. Useful commands:
                 // Select a random number in [0,n-1]
-				size_t j_1 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
-				size_t j_2 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
-				size_t j_3 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
+              size_t j_1 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
+              size_t j_2 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
+              size_t j_3 = std::min((rand() / (double)RAND_MAX) * n,(double)n-1);
 				
-				// Verify there are no similar points
-				if (j_1 == j_2 || j_1 == j_3 || j_2 == j_3) {continue;}
+              // Verify there are no similar points
+              if (j_1 == j_2 || j_1 == j_3 || j_2 == j_3) {continue;}
 				 
-				// Create a 3D point:
-				// Eigen::Vector3f P; P << x,y,z;
-				// Finding the plane equation by solving AY=B
-				Eigen::Matrix3d A; A << lastpc_[pidx[j_1]].x, lastpc_[pidx[j_1]].y, 1, 
-										lastpc_[pidx[j_2]].x, lastpc_[pidx[j_2]].y, 1,
-										lastpc_[pidx[j_3]].x, lastpc_[pidx[j_3]].y, 1;
-					
-				Eigen::Vector3d B; B << lastpc_[pidx[j_1]].z, lastpc_[pidx[j_2]].z, lastpc_[pidx[j_3]].z; 
+			
+		
+	
+              Eigen::Vector3d C; 
+              
+              float ma = (lastpc_[pidx[j_2]].y - lastpc_[pidx[j_1]].y)/(lastpc_[pidx[j_2]].x-lastpc_[pidx[j_1]].x);
+              float mb = (lastpc_[pidx[j_3]].y - lastpc_[pidx[j_2]].y)/(lastpc_[pidx[j_3]].x-lastpc_[pidx[j_2]].x);
+
+              C[0] = (ma*mb*(lastpc_[pidx[j_1]].y-lastpc_[pidx[j_3]].y)+mb*(lastpc_[pidx[j_1]].x-lastpc_[pidx[j_2]].x)-ma*(lastpc_[pidx[j_2]].x+lastpc_[pidx[j_3]].x))/(2*(mb-ma));
+              C[1] = -(1/ma)*(C[0]-(lastpc_[pidx[j_1]].x+lastpc_[pidx[j_2]].x)/2)+(lastpc_[pidx[j_1]].y+lastpc_[pidx[j_2]].y)/2;
+              C[2] = 0;
+			
+              double radius = sqrt(pow(lastpc_[pidx[j_1]].x-C[0],2.0)+pow(lastpc_[pidx[j_1]].y-C[1],2.0));
 				
-				//Verify that A is inversible
-				if (A.determinant() == 0) {continue;}
-				
-				Eigen::Vector3d Y = A.colPivHouseholderQr().solve(B);
+	
  
-				unsigned int count = 0;
+              unsigned int count = 0;
 				
-				for (unsigned j = 0; j < n; j++) {
-					Eigen::Vector3f W; W << lastpc_[pidx[j]].x, lastpc_[pidx[j]].y, lastpc_[pidx[j]].z;
-					double dist = fabs(Y[0] * W[0] + Y[1] * W[1] - W[2] + Y[2])/sqrt(pow(Y[0],2.0)+pow(Y[1],2.0)+1);
-					if (dist <= tolerance) {count++;}
-				}
+              for (unsigned j = 0; j < n; j++) {
+                Eigen::Vector3f W; W << lastpc_[pidx[j]].x, lastpc_[pidx[j]].y, lastpc_[pidx[j]].z;
+                double dist = fabs(sqrt(pow(W[0] - C[0], 2.0)+pow(W[1] - C[1], 2.0)) - radius);
+                if (dist <= tolerance) {count++;}
+              }
 				
-				if (count > best) {
-					best = count;
-					for (unsigned int k = 0; k < 3; k++) {
-						X[k] = Y[k];
-					}
-				} 
+              if (count > best) {
+                best = count;
+                for (unsigned int k = 0; k < 3; k++) {
+                  X[k] = C[k];
+                  best_radius = radius;
+                }
+              } 
 
             }
-            // At the end, make sure to store the best plane estimate in X
-            // X = {a,b,c}. This will be used for display
-			
+            //
             // END OF TODO
             
 
-			//Publishing angle between normal vector and z.
-			std_msgs::Float64 cos_msg;
-			cos_msg.data = 1 / sqrt(pow(X[0],2.0)+pow(X[1],2.0)+1);
-			ROS_INFO("angle: %.2f", cos_msg.data);
-			slope_pub_.publish(cos_msg);
+		
+	
+
 			
 			
 			//Publish the floor plane
-            ROS_INFO("Extracted floor plane: z = %.2fx + %.2fy + %.2f",
-                    X[0],X[1],X[2]);			
-			
-        
-            Eigen::Vector3f O,u,v,w;
-            w << X[0], X[1], -1.0;
-            w /= w.norm();
-            O << 1.0, 0.0, 1.0*X[0]+0.0*X[1]+X[2];
-            u << 2.0, 0.0, 2.0*X[0]+0.0*X[1]+X[2];
-            u -= O;
-            u /= u.norm();
-            v = w.cross(u);
+            ROS_INFO("Extracted circle: (x - %.2f)^2 + (y - %.2f)^2 = %.2f^2",X[0],X[1],best_radius);			
 
-            tf::Matrix3x3 R(u(0),v(0),w(0),
-                    u(1),v(1),w(1),
-                    u(2),v(2),w(2));
-            tf::Quaternion Q;
-            R.getRotation(Q);
-            
             visualization_msgs::Marker m;
             m.header.stamp = msg->header.stamp;
             m.header.frame_id = base_frame_;
-            m.ns = "floor_plane";
+            m.ns = "cylinders";
             m.id = 1;
             m.type = visualization_msgs::Marker::CYLINDER;
             m.action = visualization_msgs::Marker::ADD;
-            m.pose.position.x = O(0);
-            m.pose.position.y = O(1);
-            m.pose.position.z = O(2);
-            tf::quaternionTFToMsg(Q,m.pose.orientation);
-            m.scale.x = 1.0;
-            m.scale.y = 1.0;
+            m.pose.position.x = X[0];
+            m.pose.position.y = X[1];
+            m.pose.position.z = X[2];
+            m.pose.orientation.x = 0;
+            m.pose.orientation.y = 0;
+            m.pose.orientation.z = 0;
+            m.pose.orientation.w = 0;
+            //tf::quaternionTFToMsg(Q,m.pose.orientation);
+            m.scale.x = best_radius;
+            m.scale.y = best_radius;
             m.scale.z = 0.01;
             m.color.a = 0.5;
             m.color.r = 1.0;
@@ -168,30 +156,30 @@ class FloorPlaneRansac {
         }
 
     public:
-        FloorPlaneRansac() : nh_("~") {
+        CylindersDetection() : nh_("~") {
             nh_.param("base_frame",base_frame_,std::string("/body"));
             nh_.param("max_range",max_range_,5.0);
             nh_.param("n_samples",n_samples,1000);
             nh_.param("tolerance",tolerance,1.0);
 
-            ROS_INFO("Searching for Plane parameter z = a x + b y + c");
+            ROS_INFO("Searching for cylinders");
             ROS_INFO("RANSAC: %d iteration with %f tolerance",n_samples,tolerance);
             assert(n_samples > 0);
 
             // Make sure TF is ready
             ros::Duration(0.5).sleep();
 
-            scan_sub_ = nh_.subscribe("scans",1,&FloorPlaneRansac::pc_callback,this);
-            marker_pub_ = nh_.advertise<visualization_msgs::Marker>("floor_plane",1);
-			slope_pub_ = nh_.advertise<std_msgs::Float64>("floor_slope",100);
+            scan_sub_ = nh_.subscribe("scans",1,&CylindersDetection::pc_callback,this);
+            marker_pub_ = nh_.advertise<visualization_msgs::Marker>("cylinders",1);
+			
         }
 
 };
 
 int main(int argc, char * argv[]) 
 {
-    ros::init(argc,argv,"floor_plane_Ransac");
-    FloorPlaneRansac fp;
+    ros::init(argc,argv,"cylinders_detection");
+    CylindersDetection cd;
 
     ros::spin();
     return 0;
