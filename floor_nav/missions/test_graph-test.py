e073d4f5ff95e4b14e925b4dbd87f58a6ec9cbd5
MALLORY <gmallory@gtlpc110.georgiatech-metz.fr>
2016-10-05 16:10:37 +0200

    Updated the test_graph and test_graph-test missions to scan over 6 angles instead of 4.

--- floor_nav/missions/test_graph-test.py
+++ floor_nav/missions/test_graph-test.py
@@ -21,7 +21,10 @@ try:
     # Hand-made hamiltonian path
     node_seq = [2, 1, 4, 3, 5, 6, 11, 7, 10, 9, 8, 0]
     for node in node_seq:
-        tc.GoTo(goal_x=g.vs[node]["x"],goal_y=g.vs[node]["y"], max_velocity=0.5, k_v=1.0, max_angular_velocity=1.0)
+        tc.GoTo(goal_x=g.vs[node]["x"],goal_y=g.vs[node]["y"], max_velocity=0.35, k_v=1.0, max_angular_velocity=1.0)
+        for angle in [0.0,pi/3,2*pi/3,pi,4*pi/3,5*pi/3, 0.0]:
+			tc.Wait(duration=6.0)
+			tc.SetHeading(target=angle)
     tc.Wait(duration=1.0)
 
 except TaskException, e:
